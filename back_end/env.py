a02bb3252688e7bfb54a1b20d8973200fc0c7d3b
Matheus Oliveira <oliveira.matheusde@gmail.com>
2015-06-25 17:43:47 -0300

    env

--- back_end/env.py
+++ back_end/env.py
@@ -4,12 +4,12 @@ import os
 # DATABASE config
 os.environ['DB_ENGINE'] = 'mysql'
 os.environ['DB_NAME'] = 'luizalabs'
-os.environ['DB_USER'] = 'root'
-os.environ['DB_PASSWORD'] = 'root'
+os.environ['DB_USER'] = 'user'
+os.environ['DB_PASSWORD'] = 'pass'
 os.environ['DB_HOST'] = 'localhost'
 os.environ['DB_PORT'] = '5432'
 
 os.environ['DEBUG'] = 'True'
 
-os.environ['APP_ID'] = '896437553749930'
+os.environ['APP_ID'] = ''
 os.environ['FB_ACCESS_TOKEN'] = 'CAAMvTiH0X6oBAFOiGrt3uZAYrrgAlmcMhTbo893F4mLpZB7ly6yTuT2ZAR7sdqIbmaJvxc6xHjINeWjsQMdEdDSVu6O6ZAOGlAkHeypLSjZAYgVmuVMBqg9p90ZCRfdpZCdM7f2djitSkjWxVRYmDP4AAt0jP2icOgyy3xk7IOQw4n6daGtI5QP5oQQnu8vEqsZD'
